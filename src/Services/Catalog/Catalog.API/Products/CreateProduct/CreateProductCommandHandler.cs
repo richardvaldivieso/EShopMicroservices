f1ca491353f01d0451b90635e5e60b88487abf6d
Richard Valdivieso <richardvaldiviesom@gmail.com>
2024-06-24 14:41:20 -0500

    Implement command handler

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -1,3 +1,5 @@
+using BuildingBlocks.CQRS;
+using Catalog.API.Models;
 using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
@@ -7,15 +9,28 @@ public record CreateProductCommand(
     List<string> Category,
     string Description,
     string ImageFile,
-    decimal Price) : IRequest<CreateProductResult>;
+    decimal Price) : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        // Business logic to create a product
-        throw new NotImplementedException();
+        // create product entity from command object
+        var product = new Product
+        {
+            Name = command.Name, 
+            Category = command.Category, 
+            Description = command.Description, 
+            ImageFile = command.ImageFile, 
+            Price = command.Price
+        };
+        
+        // TODO save database
+        
+        // return CreateProductResult
+        return new CreateProductResult(Guid.NewGuid());
+
     }
 }
